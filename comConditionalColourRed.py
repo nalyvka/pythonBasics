22c1cc840d909d270d0354d9b6017aae637b66ca
nalyvka <nalyvka@pm.me>
2024-07-13 20:25:34 -0500

    experimentation with compound conditionals

---
+++ comConditionalColourRed.py
@@ -0,0 +1,5 @@
+myColour = input('Please Enter Your Colour: ')
+if (myColour == 'Red' or myColour == 'red' or myColour == 'RED'):
+    print('Your Colour Is Red')
+if (myColour != 'Red' and myColour != 'red' and myColour != 'RED'):
+    print('Your Colour Is NOT Red')
---
+++ comConditionalNumInterval.py
@@ -0,0 +1,5 @@
+myNum = int(input('Please Enter Your Number: '))
+if (myNum>=5 and myNum<=10):
+    print('Your Number Is Between 5 & 10')
+if (myNum<5 or myNum>10):
+    print('Your Number Is Not Between 5 & 10')
