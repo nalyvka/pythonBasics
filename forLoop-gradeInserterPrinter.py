14c72b33686ce94baf6b79e7a1a75554989de1bc
nalyvka <nalyvka@pm.me>
2024-07-15 21:54:13 -0500

    finds the average from inserted array of grades

--- forLoop-gradeInserterPrinter.py
+++ forLoop-gradeInserterPrinter.py
@@ -1,7 +1,12 @@
 numGrades = int(input('Please Enter The # of Grades: '))
 grades = []
+bucket = 0
 for i in range(0, numGrades, 1):
-    grade = input('Please Enter Your Grade: ')
+    grade = float(input('Please Enter Your Grade: '))
     grades.append(grade)
 for i in range(0, numGrades, 1):
+    bucket = bucket + grades[i]
+for i in range(0, numGrades, 1):
     print(grades[i])
+avg = bucket / numGrades
+print('Your Average Is: ', avg)
