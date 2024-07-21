0ef0ab561b32a287d146b69c31e39cafa46ea6a6
nalyvka <nalyvka@pm.me>
2024-07-20 23:50:02 -0500

    feat: create studentFile and adjust syntax in loop

--- studentInfo-pklRetrieve.py
+++ studentInfo-pklRetrieve.py
@@ -2,6 +2,13 @@ import pickle
 with open('studentFile.pkl', 'rb') as read:
     numStudents = pickle.load(read)
     names = pickle.load(read)
+    idValues = pickle.load(read)
     age = pickle.load(read)
     gpa = pickle.load(read)
-x = input('Which student are you interested in? ')
+
+#infinite loop
+while (0==0):
+    namePrompt = input('Which student do you want to check up ')
+    for i in range(0, numStudents, 1):
+        if (names[i]==namePrompt):
+            print(namePrompt, "'s ID is ", idValues[i], '.')
