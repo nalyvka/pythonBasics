bfbc4d856c32973c97eef53b6cc39af96fc2ebc5
nalyvka <nalyvka@pm.me>
2024-07-24 07:32:23 -0500

    Add bubble function

--- functions.py
+++ functions.py
@@ -22,14 +22,26 @@ def lowHigh(x):
         if i > highVal:
             highVal = i
     return lowVal, highVal
+def bubble(x, y):
+    swapper = 0
+    for i in range(0, x-1, 1):
+        for i in range(0, x-1, 1):
+            if y[i] < y[i+1]:
+                swapper = y[i]
+                y[i] = y[i+1]
+                y[i+1] = swapper
+    return y
+
+
 numGrades = 2
 iG = inputGrades(numGrades)
 pG = printGrades(numGrades, iG)
 aG = avGrades(numGrades, iG)
 low, high = lowHigh(iG)
 average = aG / numGrades
+bub = bubble(numGrades, iG)
 print('Your Sum Is: ', aG)
 print('Your Average Is: ', average)
 print('Your Highest Grade Is: ', high)
 print('Your Lowest Grade Is: ', low)
-
+print('Here Is Your Sorted List: ', bub)
