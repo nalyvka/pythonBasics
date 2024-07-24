81d1ef89e0fb8c17252fa39dce9589efa67db8d6
nalyvka <nalyvka@pm.me>
2024-07-24 06:19:15 -0500

    fix: print statement for grades

--- nestingFunctions.py
+++ nestingFunctions.py
@@ -1,11 +1,15 @@
+bucket = 0
 def inputGrades(x):
     grades = []
     for i in range(0, x, 1):
-        prompt = input('Please Enter Your Grades: ')
+        prompt = float(input('Please Enter Your Grades: '))
         grades.append(prompt)
-        print(grades[i])
-        return grades
-def avGrades(x):
-    
+    return grades
+def avGrades(x, y):
+    for i in range(0, x, 1):
+        bucket = bucket + grades[i]
+    return bucket
+
 numGrades = 5
 iG = inputGrades(numGrades)
+print('Your Grades Are: ', iG)
