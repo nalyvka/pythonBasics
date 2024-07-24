d928f1b8b910cce3013b483ad4e613fb54b313bf
nalyvka <nalyvka@pm.me>
2024-07-24 05:09:07 -0500

    Add inputGrades function

--- bubbleSortFunction.py
+++ bubbleSortFunction.py
@@ -1,11 +1,12 @@
-def inputGrades():
+def inputGrades(x):
+    grades = []
+    for i in range(0, x, 1):
+        prompt = input('Please Enter Your Grades: ')
+        grades.append(prompt)
+numGrades = 5
+iG = inputGrades(numGrades)
+
 def printGrades():
 def avGrades():
 def highLow():
 def bubbleSort():
-    
-inputGrades
-printGrades
-avGrades
-highLow
-bubbleSort
