ff9ef6eaa2380cc02c8acc452d01ff2bc3b7dcf7
nalyvka <nalyvka@pm.me>
2024-08-03 16:04:18 -0500

    Update print statements for readability

--- rectangularPrism.py
+++ rectangularPrism.py
@@ -17,13 +17,16 @@ class RectangularPrism:
         self.volume=self.length*self.width*self.height
         return(self.volume)
 prism1=RectangularPrism('blue', 3, 6)
+vol1 = prism1.volume(1)
 prism2=RectangularPrism('yellow', 6, 9)
+vol2 = prism2.volume(3)
 print('prism1 length = ',prism1.length)
 print('prism1 width = ', prism1.width)
 print('prism1 area =', prism1.area())
 print('prism1 perimeter = ',prism1.perimeter())
 print('prism1 diagonal = ', prism1.diagonal())
 print('prism1 colour = ',prism1.colour)
+print('prism1 volume = ',vol1)
 
 print('prism2 length = ', prism2.length)
 print('prism2 width = ', prism2.width)
@@ -31,9 +34,4 @@ print('prism2 area = ', prism2.area())
 print('prism2 perimeter = ',prism2.perimeter())
 print('prism2 diagonal = ',prism2.diagonal())
 print('prism2 colour = ', prism2.colour)
-
-vol1 = prism1.volume(1)
-vol2 = prism2.volume(3)
-
-print('prism1 volume = ',vol1)
 print('prism2 volume = ',vol2)
