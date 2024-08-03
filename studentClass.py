ebaea4d949b70bb7a09e8d9df29b83b3b3d0fee1
nalyvka <nalyvka@pm.me>
2024-08-03 16:06:39 -0500

    Outline methods for studentClass

--- studentClass.py
+++ studentClass.py
@@ -2,3 +2,8 @@ class Students:
     def __init__(self, first, last):
         self.fN = first
         self.lN = last
+    def input(self, numGrades):
+    def print(self):
+    def average(self):
+    def lowHigh(self):
+    def bubbleSort(self):
