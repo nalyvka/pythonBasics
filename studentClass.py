3ade1b4a2931b9f8337e479a2719f357b01b871c
nalyvka <nalyvka@pm.me>
2024-08-03 17:31:23 -0500

    Add insertG method

--- studentClass.py
+++ studentClass.py
@@ -1,9 +1,19 @@
 class Students:
     def __init__(self, first, last):
-        self.fN = first
-        self.lN = last
-    def input(self, numGrades):
-    def print(self):
-    def average(self):
-    def lowHigh(self):
-    def bubbleSort(self):
+        self.first = first
+        self.last = last
+    def insertG(self, numG):
+        self.numG = numG
+        self.grades = []
+        for i in range(0, numG, 1):
+            prompt = float(input('Please Enter Grade: '))
+            self.grades.append(prompt)
+        return self.grades
+
+student = Students('Ramiro', 'Santos')
+student1 = Students('Jaime', 'Carlos')
+print(student.first, student.last)
+print(student1.first, student1.last)
+
+testGrd = student.insertG(4)
+print(testGrd)
