4a45b1b6c25a514e1fcc77f39d0257b07b1291db
nalyvka <nalyvka@pm.me>
2024-08-05 18:29:42 -0500

    Rewrite studentClass

--- studentClass.py
+++ studentClass.py
@@ -2,18 +2,37 @@ class Students:
     def __init__(self, first, last):
         self.first = first
         self.last = last
-    def insertG(self, numG):
-        self.numG = numG
+
+    def insert(self,ng):
+        self.ng = ng
         self.grades = []
-        for i in range(0, numG, 1):
+        for i in range(0, ng, 1):
             prompt = float(input('Please Enter Grade: '))
             self.grades.append(prompt)
         return self.grades
+    def printGrades(self):
+        print(self.first, self.last, "'s", 'Grades Are: ')
+        print('')
+        for i in range(0, self.ng, 1):
+            print(self.grades[i])
+        print('')
+    
+    def lowHigh(self):
+        lowVal = 100
+        highVal = 0
+        for i in self.grades:
+            if i < lowVal:
+                lowVal = i
+            if i > highVal:
+                highVal = i
 
-student = Students('Ramiro', 'Santos')
-student1 = Students('Jaime', 'Carlos')
-print(student.first, student.last)
-print(student1.first, student1.last)
+#objects
+student = Students('John', 'Dee')
+student1 = Students('Emil', 'Cioran')
 
-testGrd = student.insertG(4)
-print(testGrd)
+#specify the # of grades for each student
+numG = student.insert(2)
+numG1 = student1.insert(3)
+
+test = student.printGrades()
+test1 = student1.printGrades()
