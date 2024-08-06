91f6916468678dd31cc0f8d0669b44d17b1be8c9
nalyvka <nalyvka@pm.me>
2024-08-05 19:35:54 -0500

    Remove spacing

--- studentClass.py
+++ studentClass.py
@@ -2,7 +2,6 @@ class Students:
     def __init__(self, first, last):
         self.first = first
         self.last = last
-
     def insert(self,ng):
         self.ng = ng
         self.grades = []
@@ -10,14 +9,12 @@ class Students:
             prompt = float(input('Please Enter Grade: '))
             self.grades.append(prompt)
         return self.grades
-    
     def printGrades(self):
         print(self.first, self.last, "'s", 'Grades Are: ')
         print('')
         for i in range(0, self.ng, 1):
             print(self.grades[i])
         print('')
-    
     def lowHigh(self):
         lowVal = 100
         highVal = 0
@@ -27,14 +24,12 @@ class Students:
             if i > highVal:
                 highVal = i
         return lowVal, highVal
-    
     def avg(self):
         bucket = 0
         for i in range(0, self.ng, 1):
             bucket = bucket + self.grades[i]
         average = bucket / self.ng
         return average
-    
     def bubbleSort(self):
         swapper = 0
         self.bub = self.grades
@@ -49,15 +44,10 @@ class Students:
 #objects
 student = Students('John', 'Dee')
 student1 = Students('Emil', 'Cioran')
-
-numG = student.insert(2)
-numG1 = student1.insert(3)
-
-
+numGrade = student.insert(2)
+numGrade1 = student1.insert(3)
 student.printGrades()
 student1.printGrades()
-
-
 low, high = student.lowHigh()
 print(student.first, student.last, "'s", 'Highest Grade: ', high)
 print(student.first, student.last, "'s", 'Lowest Grade: ', low)
@@ -65,9 +55,7 @@ average = student.avg()
 print(student.first, student.last, 'Has An Average Of ', average)
 sortedGrades = student.bubbleSort()
 print(student.first, student.last, "'s", 'Sorted Grades Are ', sortedGrades)
-
 print('')
-
 low1, high1 = student1.lowHigh()
 print(student1.first, student1.last, "'s", 'Highest Grade: ', high)
 print(student1.first, student1.last, "'s", 'Lowest Grade: ', low)
